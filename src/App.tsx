a9f8d8774bdd57389f44d315ce92b98e834d3e5c
Konstantin Karpov <nakosta257514@mail.ru>
2025-02-14 13:20:34 +0300

    delete

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
 
 function App() {
-  return <div>Это APP</div>;
+  return (
+    <>
+      <div>Это APP</div>
+      <div>Это DELETE</div>
+    </>
+  );
 }
 
 export default App;
