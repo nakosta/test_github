e4f2c58d755ec7821814a27dd8607aab447dca91
Konstantin Karpov <nakosta257514@mail.ru>
2025-04-26 12:41:21 +0300

    delete3

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,6 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div>Это APP</div>
-      <button>Click me please</button>
-      <input type="text" placeholder="напиши чё-нить" />
       <h1>TASK</h1>
       <h2>MAIN COMMIT</h2>
     </>
