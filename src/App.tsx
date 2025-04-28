ce0da0fa27498d87cf23d6a981665bdf2efe4543
Konstantin Karpov <nakosta257514@mail.ru>
2025-04-28 12:35:04 +0300

    button

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import "./App.css";
 
+import Button from "./componets/Button";
+
 function App() {
   return (
     <>
       <div>MAIN COMMIT</div>
       <div>develop commit</div>
+
+      <Button />
     </>
   );
 }
